a95b17fbb577d46566f7308910d0606b840d5fe4
Granuch <dylopyli@gmail.com>
2026-02-01 01:11:23 +0200

    Implement safe logging with sensitive data redaction
    Added SafeLog mechanism to redact sensitive fields in request logs using a new [SensitiveData] attribute. Introduced ISafeLoggable and SecretString for custom redaction. Applied [SensitiveData] to password in LoginCommand. SensitiveDataAttribute added to domain for marking sensitive properties. This enhances security by preventing sensitive info from being logged.

---
+++ src/BuildingBlocks/EShop.BuildingBlocks.Domain/SensitiveDataAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.BuildingBlocks.Domain
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SensitiveDataAttribute : Attribute
+    {
+    }
+}
