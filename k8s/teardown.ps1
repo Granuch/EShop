c26f02a0829203619bc404660243e8d91c0db053
Max <145479395+Granuch@users.noreply.github.com>
2026-08-15 22:14:57 +0300

    Remove comment about EShop deletion
    Removed comment about deleting EShop from the cluster.

--- k8s/teardown.ps1
+++ k8s/teardown.ps1
@@ -1,4 +1,3 @@
 #!/usr/bin/env pwsh
-# Удалить весь EShop из кластера (namespace + все ресурсы внутри)
 kubectl delete namespace eshop
 Write-Host "EShop namespace deleted. PVCs also removed." -ForegroundColor Yellow
